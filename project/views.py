2e8adadee920b0f1e3c0ec5ebbab040ff919a75e
Hoa Hoang <nickhoang11@gmail.com>
2018-01-23 20:07:39 -0800

    Welcome Page is set up

--- project/views.py
+++ project/views.py
@@ -4,8 +4,9 @@
 import sqlite3
 from functools import wraps
 
+#from forms import AddTaskForm
 from flask import Flask, flash, redirect, render_template, \
-	request, session, url_for
+    request, session, url_for, g
 
 
 # config
@@ -17,38 +18,38 @@ app.config.from_object('_config')
 # helper functions
 
 def connect_db():
-	return sqlite3.connect(app.config['DATABASE_PATH'])
+    return sqlite3.connect(app.config['DATABASE_PATH'])
 
 
 def login_required(test):
-	@wraps(test)
-	def wrap(*args, **kwargs):
-		if 'logged_in' in session:
-			return test(*args, **kwargs)
-		else:
-			flash('You need to login first.')
-			return redirect(url_for('login'))
-	return wrap
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 
 # route handlers
 
 @app.route('/logout/')
 def logout():
-	session.pop('logged_in', None)
-	flash('Goodbye!')
-	return redirect(url_for('login'))
+    session.pop('logged_in', None)
+    flash('Goodbye!')
+    return redirect(url_for('login'))
 
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME'] \
-				or request.form['password'] != app.config['PASSWORD']:
-			error = 'Invalid Credentials. Please try again.'
-			return render_template('login.html', error=error)
-		else:
-			session['logged_in'] = True
-			flash('Welcome')
-			return redirect(url_for('tasks'))
-	return render_template('login.html')
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] \
+                or request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
+            return render_template('login.html', error=error)
+        else:
+            session['logged_in'] = True
+            flash('Welcome!')
+            return redirect(url_for('tasks'))
+    return render_template('login.html')
